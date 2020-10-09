2a3882fe2732e1b9e662964287a52460872c0f42
R. Bary <rbary.lite@gmail.com>
2020-10-09 11:15:39 +0200

    splitting first example

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
-import React, { useState, useRef } from "react";
-import useOnClickOutside from "./useOnClickOutside";
+import React from "react";
+import ClickOutsideExample from "./click-outside-example";
 import "./bootstrap.min.css";
 
 // Usage
 const App = () => {
-  // Create a ref that we add to the element for which we want to detect outside clicks
-  const ref = useRef<HTMLDivElement>(null);
-  // State for our modal
-  const [isModalOpen, setModalOpen] = useState(false);
-  // Call hook passing in the ref and a function to call on outside click
-  useOnClickOutside(ref, () => setModalOpen(false));
-
   return (
     <div className="app">
-      {isModalOpen ? (
-        <div ref={ref} className="pizza-modal">
-          <span>
-            <span role="img" aria-label="pizza">
-              🍕
-            </span>{" "}
-            Hey, Click anywhere outside of here to get this pizza.
-          </span>
-        </div>
-      ) : (
-        <button className="btn btn-primary" onClick={() => setModalOpen(true)}>
-          Pizza store
-        </button>
-      )}
+      <ClickOutsideExample />
     </div>
   );
 };
